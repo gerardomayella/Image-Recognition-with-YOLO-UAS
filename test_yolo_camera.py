a85be625eaba57d40e719f731061cf693e8b6e87
gerardomayella <127300451+gerardomayella@users.noreply.github.com>
2025-06-24 18:35:16 +0700

    Add real-time YOLO model testing script with webcam integration

---
+++ test_yolo_camera.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+import cv2
+print("OpenCV version:", cv2.__version__)
+print("cv2.imshow exists:", hasattr(cv2, "imshow"))
+
+model = YOLO("runs/train/exp/weights/last.pt")
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Tidak dapat membuka kamera.")
+    exit()
+else:
+    print("Kamera berhasil dibuka.")
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Gagal membaca frame dari kamera.")
+        break
+    results = model(frame)
+    annotated_frame = results[0].plot()
+    cv2.imshow("YOLO Realtime", annotated_frame)
+
+    # Tekan 'q' untuk keluar
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
